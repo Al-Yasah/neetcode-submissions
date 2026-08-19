77170b7ffdec0f5620472fef160aecd82d95ea42
Al-Yasah <yasahfaisal@gmail.com>
2026-08-19 12:03:04 +0530

    Bulk sync: 16 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> smap = new HashMap<>();
+        HashMap<Character, Integer> tmap = new HashMap<>();
+
+        for (char ch : s.toCharArray()){
+            if (!smap.containsKey(ch)){
+                smap.put(ch,1);
+            }else{
+                int count = smap.get(ch);
+                smap.put(ch,count+1);
+            }
+        }
+        for (char ch : t.toCharArray()){
+            if (!tmap.containsKey(ch)){
+                tmap.put(ch,1);
+            }else{
+                int count = tmap.get(ch);
+                tmap.put(ch,count+1);
+            }
+        }
+
+        if (smap.size() == tmap.size()){
+            for (Map.Entry<Character,Integer> entry : smap.entrySet()){
+                char key = entry.getKey();
+                if (!(entry.getValue() == tmap.get(key))){
+                    return false;
+                }
+            } 
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+}
---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int len = s.length();
+        int left = 0;
+        int right = len-1;
+        while (left<right){
+            char l= Character.toLowerCase(s.charAt(left));
+            char r = Character.toLowerCase(s.charAt(right));
+
+            while (!Character.isLetterOrDigit(l)){
+                left++;
+                l=Character.toLowerCase(s.charAt(left));
+            }
+            while (!Character.isLetterOrDigit(r)){
+                right--;
+                r=Character.toLowerCase(s.charAt(right));
+            }
+
+            if (l!=r){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
